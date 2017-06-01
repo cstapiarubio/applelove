151f3eb7547df0ad3b52286ab4c7992ed94016e5
CarolinaTapia <cstapiarubio@gmail.com>
2017-06-01 10:47:47 -0400

    trabajo final, no me funciono las ventanas emergertes de las fotos de la seccion work

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
  * Archivo principal de funcionalidad de JS
  */
 
+ //funcion que hizo la Blanca para las imaganes de la seccion services
+
  (function main(){
  	var boxes= Array.from(document.getElementsByClassName('box-services'));
  	var modal=document.getElementById("box-services-modal");
@@ -41,4 +43,33 @@ close.addEventListener("click", function(){
 	modal.classList.add("hide");
 });*/
 });
+ })();
+
+ //funcion para las ventanas emergentes de las nueve imagenes de la seccion work
+
+ (function main-fotos(){
+ 	var cajas=Array.from(document.getElementsByClassName("col-4"));
+ 	var cajas-fotos=document.getElementById("box-services-container");
+ 	var containerModal, closeContainer;
+
+ 	cajas.forEach(function(cajabox){
+ 		cajabox.addEventListener("click", function(){
+ 			cajas-fotos.innerHTML="";
+ 			containerModal=document.createElement("div");
+ 			containerModal.classList.add("modal-container");
+ 			containerModal.innerHTML=cajabox.innerHTML;
+ 			cajas-fotos.appendChild(containerModal);
+ 			cajas-fotos.classList.remove("hide");
+ 			closeContainer=document.createElement("div");
+ 			closeContainer.classList.add("closeContainer");
+ 			imag=document.createElement("img");
+ 			imag.setAttribute("src", "http://static1.squarespace.com/static/55a420c9e4b01d62e11ae524/t/573bf9cc8259b5b384b219e8/1475714273597/");
+ 			closeContainer.appendChild(imag);
+ 			cajas-fotos.appendChild(closeContainer);
+ 			closeContainer.addEventListener("click", function(){
+ 				cajas-fotos.classList.add("hide");
+ 			});
+ 		});
+ 	});
+
  })();
